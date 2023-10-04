6cc2afbf4fd7d9e341f9debaea7b52f5dc66df8b
OOP_Reyes <145189756+goldygoldss@users.noreply.github.com>
2023-10-04 22:10:34 +0800

    Add files via upload

---
+++ SavingsAccount.java
@@ -0,0 +1,43 @@
+
+class SavingsAccount {
+    private double balance;
+    private static double interestRate = 0;
+
+    public SavingsAccount() {
+        balance = 0;
+    }
+
+    public static void setInterestRate(double newRate) {
+        interestRate = newRate;
+    }
+
+    public static double getInterestRate() {
+        return interestRate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return amount;
+        } else {
+            return 0;
+        }
+    }
+
+    public void addInterest() {
+        double interest = balance * interestRate;
+        balance += interest;
+    }
+
+    public static void showBalance(SavingsAccount account) {
+        System.out.println("Current balance: $" + account.getBalance());
+    }
+}
